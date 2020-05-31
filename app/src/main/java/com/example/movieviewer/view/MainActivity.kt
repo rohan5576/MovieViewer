c4f1d825061a11a9564b6c6d30e34bd6b77ac380
rohan5576 <lodhirohan138@gmail.com>
2020-05-31 19:14:41 +0530

    Renamed images.

--- app/src/main/java/com/example/movieviewer/view/MainActivity.kt
+++ app/src/main/java/com/example/movieviewer/view/MainActivity.kt
@@ -2,13 +2,25 @@ package com.example.movieviewer.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.example.movieviewer.R
 
 class MainActivity : AppCompatActivity() {
+    /**
+     * NavController instance.
+     */
+    private lateinit var navigationController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navigationController = Navigation.findNavController(this, R.id.fragment)
+        NavigationUI.setupActionBarWithNavController(this, navigationController)
     }
 
-
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navigationController, null)
+    }
 }
